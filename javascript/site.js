252cd2b2ca9e68aac60be7d7e505cac63cb8a973
Phi Hai Nguyen <phihai93@gmail.com>
2019-05-28 12:02:33 -0700

    update

--- javascript/site.js
+++ javascript/site.js
@@ -1,10 +1,10 @@
+$(".hamburger").click(function() {
+  $(".hamburger").toggleClass(" is-active");
+  $(".bg-text").fadeToggle(1000);
+});
 
-    $(".hamburger").click(function () {
-        $(".hamburger").toggleClass(" is-active"); 
-        $(".bg-text").fadeToggle(1000);
-    })
-    
+document.body.className = "fade";
 
-    
-
-
+document.addEventListener("DOMContentLoaded", function(e) {
+  document.body.className = "";
+});
